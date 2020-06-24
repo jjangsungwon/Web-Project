57d15d648a072cdc9823f0086919008d5bdbc46e
chanhuiseok <chanhuicom@gmail.com>
2020-06-25 02:17:28 +0900

    크롤링 데이터 표시 테스트 완료(DB 테스트 필요)

--- app.py
+++ app.py
@@ -1,11 +1,95 @@
 from flask import Flask, render_template
+######(추후 삭제) 
+import requests
+from bs4 import BeautifulSoup
+######(추후 삭제) 
 
 app = Flask(__name__)
 
+####### 크롤링 데이터 출력 테스트 (추후 삭제) ########
+
+
+# (1) - 전체 공지사항
+
+req = requests.get("http://knu.ac.kr/wbbs/wbbs/bbs/btin/list.action?bbs_cde=1&menu_idx=67")
+soup = BeautifulSoup(req.text, 'html.parser')
+# 제목
+title_1 = []
+for i in soup.select('div.board_list > table > tbody > tr > td.subject'):
+    title_1.append(i.text.strip())  # 공백 제거
+# 등록일
+date_1 = []
+for i in soup.select('div.board_list > table > tbody > tr > td.date'):
+    date_1.append(i.text.strip())
+# 링크
+link_1 = []
+for i in soup.select('div.board_list > table > tbody > tr > td.subject'):
+    temp = "http://knu.ac.kr/wbbs/wbbs/bbs/btin/viewBtin.action?" + i.find("a")['href']
+    link_1.append(temp)
+
+# (2) - 학사공지
+req = requests.get("http://knu.ac.kr/wbbs/wbbs/bbs/btin/stdList.action?menu_idx=42")
+soup = BeautifulSoup(req.text, 'html.parser')
+# 제목
+title_2 = []
+for i in soup.select('div.board_list > table > tbody > tr > td.subject'):
+    title_2.append(i.text.strip())  # 공백 제거
+# 등록일
+date_2 = []
+for i in soup.select('div.board_list > table > tbody > tr > td.date'):
+    date_2.append(i.text.strip())
+
+# 링크
+# 링크를 javascript:doRead('13997899', '000000', '812', 'row'); 형태로 줘서 앞에 숫자 2개를 이용하기 위해 별도의 과정을 추가함
+link_2 = []
+for i in soup.select('div.board_list > table > tbody > tr > td.subject'):
+    val = i.find("a")['href']
+    val = val[18:]
+    val = val[:-2]
+    val = val.split(' ')
+    no1 = val[0][1:-2]
+    no2 = val[1][1:-2]
+    temp = "http://knu.ac.kr/wbbs/wbbs/bbs/btin/stdViewBtin.action?btin.doc_no=" + val[0][1:-2] + "&btin.appl_no=" + val[1][1:-2] + "&btin.page=1&btin.search_type=&btin.search_text=&popupDeco=&btin.note_div=top&menu_idx=42"
+    link_2.append(temp)
+
+for i in range(len(title_1)):
+    print("Title", title_1[i])
+    print("Date", date_1[i])
+    print("Link", link_1[i])
+
+##########################################
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html"
+        )
+
+@app.route('/acainfo')
+def acainfos():
+    return render_template("acainfo.html")
+
+@app.route('/mealmenu')
+def mealmenus():
+    return render_template("mealmenu.html")    
+
+@app.route('/sitelink')
+def sitelinks():
+    return render_template("sitelink.html")        
+
+@app.route('/notice')
+def notices():
+    return render_template(
+        "notice.html",
+        news_title=[ title_1[i] for i in range(len(title_1))]
+        )       
+
+@app.route('/schoolmap')
+def schoolmaps():
+    return render_template("schoolmap.html")           
+
+
+
 
 
 if __name__ == '__main__':
